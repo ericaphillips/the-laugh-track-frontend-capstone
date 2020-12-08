13a16f8a5b3f4567ec6c172f0a360382dcf6d52f
ericaphillips <ericaphill@gmail.com>
2020-12-08 15:37:08 -0600

    added login and registration functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { Route, Redirect } from "react-router-dom"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
-}
+export const App = () => (
+  <>
+      <Route render={() => {
+          // The user id is saved under the key app_user_id in local Storage. Change below if needed!
+            if (localStorage.getItem("app_user_id")) {
+                return (
+                    <>
+                        //Components that are rendered when the user is authenticated go inside this React fragment
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
 
-export default App;
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
+    </>
+    )
