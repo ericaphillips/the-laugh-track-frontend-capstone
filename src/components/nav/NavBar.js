1728c2f3b673e49f3012425a12929a5e785d5506
ericaphillips <ericaphill@gmail.com>
2020-12-09 14:52:54 -0600

    added specials list

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from 'react-router-dom'
+import { Logout } from '../auth/Logout'
 import "./NavBar.css"
 
 export const NavBar = (props) => {
@@ -17,6 +18,10 @@ export const NavBar = (props) => {
             <li className="navBar__item">
                 <Link className="navBar__link" to="/profile">My Profile</Link>
             </li>
+            <button className="logout--button" onClick={Logout}>
+                            Log Out
+                        </button>
         </ul>
+        
     )
 }
